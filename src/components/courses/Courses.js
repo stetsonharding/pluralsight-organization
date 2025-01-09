6f2946a70aae7c6f60815b6170a85059bac58fa8
stetson harding <stetson2001@msn.com>
2025-01-09 15:56:39 -0800

    API data hydration.

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -1,42 +1,37 @@
-import React, { useState } from 'react'
+import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import * as courseActions from "../../redux/actions/courseActions"
 import PropTypes from 'prop-types'
 
 
 
-const Courses = ({courses, createCourse }) => {
+const Courses = ({ courses, loadCourses }) => {
 
-//Can use these instead of mapStateToProps and mapDispatchToProps if needed. Make sure to import these hooks.
+  //Can use these instead of mapStateToProps and mapDispatchToProps if needed. Make sure to import these hooks.
   // const dispatch = useDispatch();
   // const courses = useSelector( state => state.courses)
 
-  const [course, setCourse] = useState({title: ""})
+  useEffect( async () => {
+loadCourses().catch(error => {
+  alert("Loading courses failed" + error)
+});
+  },[])
 
-  const handleChange = (e) => {
-    setCourse({...course, title: e.target.value})
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createCourse( course )
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <h2>Courses</h2>
-      <h3>Add Course</h3>
-      <input type='text' onChange={handleChange} value={course.title} />
-      <input type='submit' value="Save" />
       {courses.map((course) => (
         <div key={course.title}><p>{course.title}</p></div>
       ))}
-    </form>
+    </>
+
   )
 }
 
 Courses.propTypes = {
   createCourse: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired
 };
 
@@ -53,7 +48,11 @@ const mapDispatchToProps = (dispatch) => {
   return {
     createCourse(course) {
       dispatch(courseActions.createCourse(course))
+    },
+    loadCourses(courses) {
+      dispatch(courseActions.loadCourses(courses))
     }
+
   }
 }
 
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,23 @@
 import * as types from "./actionTypes"
+import * as courseApi from "../../api/courseApi"
+
 
 
 export function createCourse(course) {
     return {type: types.CREATE_COURSE, course}
+}
+
+
+//Creation of Thunk
+export function loadCourses() {
+    //A thunk must return a function. Needs to look like this signature.
+    //Thunk middlware passes dipatch as an argument to our thunk
+    return function(dispatch) {
+        //Load courses from api
+        return courseApi.getCourse().then(courses => {
+            dispatch({type: types.LOAD_COURSES_SUCCESS, courses })
+        }).catch(error => {
+            throw error
+        })
+    }
 }
